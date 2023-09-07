5174fd2d55e96fbfe6ebb1aa468a0e96b42c93d3
Adejuwon Ayodimeji <austinjuwon@gmail.com>
2023-09-07 01:15:56 +0100

    Added Twitter Processing

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,23 +3,37 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.twitter import scrape_user_tweets
 
+name = "Prosper Otemuyiwa"
 if __name__ == "__main__":
     print("Hello LangBaba")
 
     # Using agent to get search online for the name and fetch the linkedin profile url of the name
-    linkedin_profile_url = linkedin_lookup_agent(name="Adejuwon Ayodimeji")
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+
+    # scraping the linkedin profile page
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+
+    # scraping the twitter page
+    twitter_username = twitter_lookup_agent(name=name)
+    print(twitter_username)
+    tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
 
     # A template of what we want the llm to do, given an information: scraped data from the linkedin profile
     summary_template = """
-        given the information {information} about a person from I want you to create:
+        given the LinkedIn information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        3. A topic that may interest them
+        4. 2 creative Ice breakers to open a conversation with them
     """
 
     prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["linkedin_information", "twitter_information"],
+        template=summary_template,
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
@@ -27,7 +41,5 @@ if __name__ == "__main__":
     # chaining the llm and the prompt template
     chain = LLMChain(llm=llm, prompt=prompt_template)
 
-    # scraping the linkedin profile page
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
-
-    print(chain.run(information=linkedin_data))
+    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    # print(tweets)
